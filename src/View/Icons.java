d0c029f1fd44b82c1b463e4a3fbf44687534272f
Roham <rzninvo@gmal.com>
2019-06-21 01:31:30 +0430

    Added a minor aspect ratio saver Added minimum size limiter and gave all the panels their correct size Next step: adding icons and the beginning of GUI action listeners

--- src/View/Icons.java
+++ src/View/Icons.java
@@ -6,6 +6,17 @@ import java.awt.*;
 public class Icons {
     static ImageIcon JPOTIFY_ICON = new ImageIcon("src/View/Icons/JPotify.png");
     static ImageIcon VOLUME_ICON = new ImageIcon("src/View/Icons/Volume.png");
+    static ImageIcon BROWSE_ICON = new ImageIcon("src/View/Icons/browse.png");
+    static ImageIcon BROWSE_FOCUSED_ICON = new ImageIcon("src/View/Icons/browse_focused.png");
+    static ImageIcon DOWN_ARROW_ICON = new ImageIcon("src/View/Icons/down_arrow.png");
+    static ImageIcon HOME_ICON = new ImageIcon("src/View/Icons/home.png");
+    static ImageIcon HOME_FOCUSED_ICON = new ImageIcon("src/View/Icons/home_focused.png");
+    static ImageIcon LEFT_ARROW_ICON = new ImageIcon("src/View/Icons/left_arrow.png");
+    static ImageIcon MENU_ICON = new ImageIcon("src/View/Icons/menu.png");
+    static ImageIcon RADIO_ICON = new ImageIcon("src/View/Icons/radio.png");
+    static ImageIcon RIGHT_ARROW_ICON = new ImageIcon("src/View/Icons/right_arrow.png");
+    static ImageIcon UP_ARROW_ICON = new ImageIcon("src/View/Icons/up_arrow.png");
+    static ImageIcon USER_ICON = new ImageIcon("src/View/Icons/user.png");
 
     public static ImageIcon rescaleIcon(ImageIcon imageIcon, int width, int height) {
         Image image = imageIcon.getImage();
--- src/View/TransparentButton.java
+++ src/View/TransparentButton.java
@@ -1,10 +1,12 @@
 package View;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class TransparentButton extends JButton {
     TransparentButton(String text, ImageIcon imageIcon){
         super(text, imageIcon);
+        setForeground(new Color(175, 175, 175));
         this.setFocusPainted(false);
         this.setBorderPainted(false);
         this.setOpaque(false);
