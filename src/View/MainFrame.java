97706691375df35e657ff8b3e81237cd6710ef25
Roham <rzninvo@gmal.com>
2019-06-20 23:53:11 +0430

    Added a minor aspect ratio saver Added minimum size limiter and gave all the panels their correct size Next step: adding icons and the beginning of GUI action listeners

--- src/View/MainFrame.java
+++ src/View/MainFrame.java
@@ -8,7 +8,7 @@ import java.awt.event.ComponentEvent;
 public class MainFrame extends JFrame {
     private final String WINDOW_TITLE = "JPotify";
     private final int WIDTH = 1920;
-    private final int HEIGHT = 1080;
+    private final int HEIGHT = 1030;
     private final int X = 0;
     private final int Y = 0;
     static MainPanel mainPanel = new MainPanel();
@@ -19,15 +19,19 @@ public class MainFrame extends JFrame {
         FrameComponent frame = new FrameComponent(new Insets(5, 5, 5, 5));
         this.setIconImage(Toolkit.getDefaultToolkit().getImage("src/View/Icons/JPotify.png"));
         this.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
-        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
         this.setLocationRelativeTo(null);
         this.setLayout(null);
         this.add(frame);
         this.add(mainPanel);
+        this.setMinimumSize(new Dimension(800, 600));
         this.addComponentListener(new ComponentAdapter() {
             @Override
             public void componentResized(ComponentEvent e) {
                 Dimension sizeIn = ((JFrame) e.getComponent()).getContentPane().getSize();
+                if (sizeIn.getWidth() < 800)
+                    sizeIn.width = 800;
+                if (sizeIn.getHeight() < 600)
+                    sizeIn.height = 600;
                 frame.setSize(sizeIn);
                 mainPanel.setSize(sizeIn);
             }
@@ -37,7 +41,7 @@ public class MainFrame extends JFrame {
         frame.addMouseMotionListener(controller);
         mainPanel.addMouseMotionListener(new BackgroundComponentDragger(this));
         Dimension dimPant = Toolkit.getDefaultToolkit().getScreenSize();
-        this.setBounds(0, 0, dimPant.width, dimPant.height);
+        this.setBounds(0, 0, dimPant.width, dimPant.height-40);
         this.setUndecorated(true);
         this.setVisible(true);
     }
--- src/View/MainPanel.java
+++ src/View/MainPanel.java
@@ -1,6 +1,7 @@
 package View;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainPanel extends JPanel {
     static WestPanel westPanel = new WestPanel();
@@ -11,18 +12,19 @@ public class MainPanel extends JPanel {
     MainPanel() {
         super();
         GroupLayout layout = new GroupLayout(this);
+        this.setBackground(new Color(24, 24, 24));
         this.setLayout(layout);
         layout.setHorizontalGroup(layout.createParallelGroup()
                 .addGroup(layout.createSequentialGroup()
-                        .addComponent(westPanel, 340, 340, Short.MAX_VALUE)
-                        .addComponent(centrePanel, 600, 1205, Short.MAX_VALUE)
-                        .addComponent(friendsPanel, 0, 375, Short.MAX_VALUE))
+                        .addComponent(westPanel, 215, 215, Short.MAX_VALUE)
+                        .addComponent(centrePanel, 600, 1450, Short.MAX_VALUE)
+                        .addComponent(friendsPanel, 255, 255, Short.MAX_VALUE))
                 .addComponent(playPanel, 940, 1920, Short.MAX_VALUE));
         layout.setVerticalGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup()
-                        .addComponent(westPanel, 500, 855, Short.MAX_VALUE)
-                        .addComponent(centrePanel, 500, 855, Short.MAX_VALUE)
-                        .addComponent(friendsPanel, 500, 855, Short.MAX_VALUE))
-                .addComponent(playPanel, 225, 225, Short.MAX_VALUE));
+                        .addComponent(westPanel, 500, 952, Short.MAX_VALUE)
+                        .addComponent(centrePanel, 500, 952, Short.MAX_VALUE)
+                        .addComponent(friendsPanel, 500, 952, Short.MAX_VALUE))
+                .addComponent(playPanel, 88, 88, Short.MAX_VALUE));
     }
 }
